12aca06dcfa438057ae5f5ed0ad7c19cf809ac1a
JasonLamv-t <jasonlamv-t@hotmail.com>
2023-01-24 23:37:41 +0800

    update & fix: adjust code snippet and fix the problem that the defalut value does not take effect.

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-function generateToc(options = {
-    callback: function () { return; },
-    depth: 2,
-    id: false
-}) {
-    function filterHeading(node) {
-        return node.type === 'heading' && node.depth <= options.depth;
+export default function generateToc(options = { callback, depth, id }) {
+  (options.callback =
+    options.callback ??
+    function () {
+      return;
+    }),
+    (options.depth = options.depth ?? 2);
+  options.id = options.id ?? false;
+
+  function filterHeading(node) {
+    return node.type === 'heading' && node.depth <= options.depth;
+  }
+
+  function mapHeading(header) {
+    let res = {
+      depth: header.depth,
+      value: header.children[0].value,
+    };
+    if (options.id) {
+      res.id = header.data.id;
     }
-    function mapHeading(header) {
-        let res = {
-            depth: header.depth,
-            value: header.children[0].value,
-        }
-        if (options.id) {
-            res.id = header.data.id;
-        }
-        return res;
-    }
-	function parse(root) {
-        const headers = root.children
-            .filter(filterHeading)
-            .map(mapHeading)
-        options.callback(headers);
-    }
-    return parse;
+    return res;
+  }
+
+  function parse(root) {
+    const headers = root.children.filter(filterHeading).map(mapHeading);
+    options.callback(headers);
+  }
+
+  return parse;
 }
-
-
-module.exports = generateToc;
